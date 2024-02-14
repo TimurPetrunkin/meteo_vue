6634938c8cca2382c94770dde30657f8d201af50
Timur Petrunkin <timur046@gmail.com>
2024-02-14 17:34:02 +0300

    14.02.2024

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import WeatherHighlights from './components/WeatherHighlights.vue'
+import WeatherSummary from './components/WeatherSummary.vue'
+import { BASE_URL, WEATHER_API } from './constants'
+
+const city = ref('Paris')
+const weatherInfo = ref(null)
+
+function getWeather() {
+  fetch(`${BASE_URL}?q=${city.value}&appid=${WEATHER_API}`)
+  .then((response) => response.json())
+  .then((data)=> weatherInfo.value = data)
+}
+
+onMounted(getWeather)
+</script>
 
 <template>
   <div class="page">
@@ -9,129 +25,18 @@
             <section class="section section-left">
               <div class="info">
                 <div class="city-inner">
-                  <input type="text" class="search" />
+                  <input 
+                  v-model="city" 
+                  type="text" 
+                  class="search" 
+                  @keyup.enter='getWeather'
+                  />
                 </div>
-                <div class="summary">
-                  <div
-                    style="background-image: url('./img/weather-main/thunderstorm.png')"
-                    class="pic-main"
-                  ></div>
-                  <div class="weather">
-                    <div class="temp">14 °C</div>
-                    <div class="weather-desc text-block">Thunderstorm</div>
-                  </div>
-                  <div class="city text-block">Paris, FR</div>
-                  <div class="date text-block">Thu, March 16, 2023</div>
-                </div>
+                <WeatherSummary />
               </div>
             </section>
             <section class="section section-right">
-              <div class="section highlights">
-                <div class="title">Today's Highlights</div>
-                <div class="highlights-wrapper">
-                  <div class="highlight">
-                    <div class="card">
-                      <div class="card-title">Wind</div>
-                      <div class="card-pic card-pic--wind"></div>
-                      <div class="card-info">
-                        <div class="card-justify">
-                          <div class="info-main">
-                            <div class="info-main-num">3.6</div>
-                            <div class="info-main-text">m/s</div>
-                          </div>
-                          <div class="info-main">
-                            <div class="info-main-num">350</div>
-                            <div class="info-main-text">deg</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-small">
-                      <div class="card-small-title">Wind gusts</div>
-                      <div class="card-small-info">
-                        <div class="card-small-data">
-                          <div class="info-main-num">8.4</div>
-                          <div class="info-main-text">m/s</div>
-                        </div>
-                        <div class="card-small-hint">
-                          <div class="card-small-pic card-small-pic--wind"></div>
-                          <div class="card-small-text text-egorova">
-                            Learn
-                            <a
-                              href="https://www.windy.com/articles/weather-phenomena-what-s-the-difference-between-sustained-winds-and-wind-gusts-10390?satellite,7.787,115.115,5"
-                              target="_blank"
-                              >more</a
-                            >
-                            about gusts
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="highlight">
-                    <div class="card">
-                      <div class="card-title">Pressure</div>
-                      <div class="card-pic card-pic--pressure"></div>
-                      <div class="card-info">
-                        <div class="card-centered">
-                          <div class="info-main">
-                            <div class="info-main-num">765</div>
-                            <div class="info-main-text">mm</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-small">
-                      <div class="card-small-title">Feels like</div>
-                      <div class="card-small-info">
-                        <div class="card-small-data">
-                          <div class="info-main-num">21</div>
-                          <div class="info-main-text">°C</div>
-                        </div>
-                        <div class="card-small-hint">
-                          <div
-                            class="card-small-pic card-small-pic--margin card-small-pic--pressure"
-                          ></div>
-                          <div class="card-small-text">How hot or cold it really feels</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="highlight">
-                    <div class="card">
-                      <div class="card-title">Sunrise and sunset</div>
-                      <div class="card-pic card-pic--sun"></div>
-                      <div class="card-info">
-                        <div class="states">
-                          <div class="state">
-                            <div class="state-pic"></div>
-                            <div class="state-title">Sunrise</div>
-                            <div class="state-time">07:31:42</div>
-                          </div>
-                          <div class="state">
-                            <div class="state-pic state-pic--flipped"></div>
-                            <div class="state-title">Sunset</div>
-                            <div class="state-time">18:34:19</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-small">
-                      <div class="card-small-title">Cloudiness</div>
-                      <div class="card-small-info">
-                        <div class="card-small-data">
-                          <div class="info-main-num">80</div>
-                          <div class="info-main-text">%</div>
-                        </div>
-                        <div class="card-small-hint">
-                          <div class="card-small-pic card-small-pic--sun"></div>
-                          <div class="card-small-text">The sky fraction obscured by clouds</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <WeatherHighlights />
             </section>
           </div>
           <div class="sections">
@@ -184,7 +89,7 @@
 </template>
 
 <style lang="scss" scoped>
-@import '/assets/scss/main.scss';
+@import './assets/styles/main.scss';
 .page {
   position: relative;
   display: flex;
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const WEATHER_API = 'efb9a63f62db098c11f93f65554bf49d';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
